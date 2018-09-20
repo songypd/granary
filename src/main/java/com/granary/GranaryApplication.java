4b830f802a24505d97fe864cce373f55dd2c7456
songypd <galaxy_songypd@163.com>
2018-09-20 16:24:12 +0800

    firstpush

---
+++ src/main/java/com/granary/GranaryApplication.java
@@ -0,0 +1,12 @@
+package com.granary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranaryApplication.class, args);
+	}
+}
